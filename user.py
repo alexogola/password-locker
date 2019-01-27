3af6eead826c77944396124bb45018b90f3ef332
Alex Ogola <ogollaokumu@gmail.com>
2019-01-27 23:35:41 +0300

    create new instances of a class user

--- user.py
+++ user.py
@@ -1,2 +1,8 @@
 # import unittest
 class User :
+
+    user_list = []
+    def __init__ (self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password = password
