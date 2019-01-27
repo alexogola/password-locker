19400a8acd3e2ad19f77c91f5331859fba4090ba
Alex Ogola <ogollaokumu@gmail.com>
2019-01-27 23:40:46 +0300

    add docstrings

--- user.py
+++ user.py
@@ -1,8 +1,17 @@
 # import unittest
 class User :
 
+    """
+    Class that generates new instances of users
+    """
+
     user_list = []
     def __init__ (self, account, username, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+        '''
+
         self.account = account
         self.username = username
         self.password = password
