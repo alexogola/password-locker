d4c360fb5e59fcb7b8b7034c5348629474e745a7
Alex Ogola <ogollaokumu@gmail.com>
2019-01-28 00:12:46 +0300

    add more functions

--- p_locker.py
+++ p_locker.py
@@ -13,3 +13,21 @@ def save_user(user):
     Function to save user
     '''
     user.save_user()
+#To find user
+def find_user(account):
+    '''
+    Function to find user
+    '''
+    def find_user(account):
+#To check existing user
+def check_existing_user(account):
+    '''
+    Function to check existing user
+    '''
+    return User.user_exist(account)
+#To display users
+def display_users():
+    '''
+    Function to display users
+    '''
+    return User.display_users()
