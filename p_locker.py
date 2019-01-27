647f2860ac562d36f1ab25149b666aa5ca16bd17
Alex Ogola <ogollaokumu@gmail.com>
2019-01-27 23:59:27 +0300

    add function to create a new account

--- p_locker.py
+++ p_locker.py
@@ -1,2 +1,9 @@
 import pyperclip
 from user import User
+#To add an account
+def create_account(account,username,password):
+    '''
+    Function to create a new account
+    '''
+    new_user = User(account,username,password)
+    return new_user
